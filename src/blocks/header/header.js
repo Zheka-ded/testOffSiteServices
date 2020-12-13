d007b6f5e45b9462f25014262766bfecfb667da2
Zheka-ded <11vitamin11@rambler.ru>
2020-12-13 11:14:16 +0200

    add menu close

--- src/blocks/header/header.js
+++ src/blocks/header/header.js
@@ -1,4 +1,14 @@
-document.querySelector('.header__nav-mobile').onclick = function() {
-    document.querySelector('.header__nav-ul').classList.toggle('mobile__flex')
-    this.textContent !== 'X' ? this.textContent = 'X' : this.innerHTML = '&#9776;';
+const headerNavBtn = document.querySelector('.header__nav-mobile');
+const headerNavList = document.querySelector('.header__nav-ul');
+const headerNavLink = document.querySelectorAll('.header__nav-link');
+const openMenu = '&#9776;'
+
+headerNavBtn.onclick = function() {
+    headerNavList.classList.toggle('mobile__flex')
+    this.textContent !== 'X' ? this.textContent = 'X' : this.innerHTML = openMenu;
+
+    headerNavLink.forEach(item => item.onclick = () => {
+        headerNavList.classList.remove('mobile__flex');
+        headerNavBtn.innerHTML = openMenu;
+    })
 }
