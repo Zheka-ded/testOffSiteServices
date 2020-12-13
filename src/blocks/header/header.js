c41ff22b6f5fb461c251e5ec8e26d7bbe7cdb90c
Zheka-ded <11vitamin11@rambler.ru>
2020-12-13 11:30:31 +0200

    thats all

--- src/blocks/header/header.js
+++ src/blocks/header/header.js
@@ -1,14 +1,19 @@
 const headerNavBtn = document.querySelector('.header__nav-mobile');
 const headerNavList = document.querySelector('.header__nav-ul');
 const headerNavLink = document.querySelectorAll('.header__nav-link');
-const openMenu = '&#9776;'
+const openMenu = '&#9776;';
 
 headerNavBtn.onclick = function() {
     headerNavList.classList.toggle('mobile__flex')
     this.textContent !== 'X' ? this.textContent = 'X' : this.innerHTML = openMenu;
-
+    
     headerNavLink.forEach(item => item.onclick = () => {
-        headerNavList.classList.remove('mobile__flex');
-        headerNavBtn.innerHTML = openMenu;
+
+        if(headerNavList.classList.contains('mobile__flex')){
+            headerNavList.classList.remove('mobile__flex');
+            headerNavBtn.innerHTML = openMenu;
+        }
+
     })
+
 }
